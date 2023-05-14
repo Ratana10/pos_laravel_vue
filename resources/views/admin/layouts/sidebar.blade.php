44d98400ec9959dbfac020eeedcb7d69b5b78159
Ratana10 <sanratana18@gmail.com>
2023-05-14 10:55:08 +0700

    config router

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,39 @@
+<div class="user-panel mt-3 pb-3 mb-3 d-flex">
+   <div class="image">
+      <img src="https://adminlte.io/themes/v3/dist/img/user2-160x160.jpg" class="img-circle elevation-2"
+         alt="User Image">
+   </div>
+   <div class="info">
+      <a href="#" class="d-block">Alexander Pierce</a>
+   </div>
+</div>
+
+<nav class="mt-2">
+   <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+      <li class="nav-item menu-open">
+         <a href="#" class="nav-link active">
+            <i class="nav-icon fas fa-tachometer-alt"></i>
+            <p>
+               Dashboard
+            </p>
+         </a>
+      </li>
+      <li class="nav-item">
+         <a href="#" class="nav-link">
+            <i class="nav-icon fas fa-th"></i>
+            <p>
+               Customer
+            </p>
+         </a>
+      </li>
+      <li class="nav-item">
+         <a href="#" class="nav-link">
+            <i class="nav-icon fas fa-th"></i>
+            <p>
+               Supplier
+            </p>
+         </a>
+      </li>
+   </ul>
+</nav>
