efc2cce76226f89d2e78823bf696c34fd11d7cea
Ratana10 <sanratana18@gmail.com>
2023-05-14 11:34:46 +0700

    modify app layout

---
+++ resources/js/Pages/Cutomers/Customer.vue
@@ -0,0 +1,13 @@
+<template>
+   <div>
+      this is customer
+   </div>
+</template>
+<script>
+export default {
+   
+}
+</script>
+<style>
+   
+</style>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,9 +1,15 @@
-import Dashboard from './Components/Dashboard.vue'
+import Dashboard from './Components/Dashboard.vue';
+import Customer from './Pages/Cutomers/Customer.vue';
 export default
 [
    {
       path: '/admin/dashboard',
-      name: 'dashboard',
+      name: 'admin.dashboard',
       component: Dashboard,
+   },
+   {
+      path: '/admin/customers',
+      name: 'admin.customers',
+      component: Customer,
    },
 ]
