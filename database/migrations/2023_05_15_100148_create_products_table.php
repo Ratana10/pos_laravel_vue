f0eae30498c0df7cbad352067012aba3a8821578
Ratana10 <sanratana18@gmail.com>
2023-05-15 17:15:13 +0700

    migrate product table

---
+++ database/migrations/2023_05_15_100148_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->nullable();
+            $table->string('name');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->decimal('unit_price',5,2)->default(0)->nullable();
+            $table->decimal('price',5,2)->default(0)->nullable();
+            $table->bigInteger('quantity')->default(0);
+            $table->string('image')->nullable();
+            $table->string('barcode')->nullable();
+            $table->boolean('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
